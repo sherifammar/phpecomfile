04bcb6de84e974c27833572b7aa8583ae77f0821
sherif ammar <sherifsobhy.nice@hotmail.com>
2024-07-05 19:26:27 +0300

    change coupon

--- cart/coupon.php
+++ cart/coupon.php
@@ -2,13 +2,14 @@
 include "../connect.php";
 
 
-$now= date("Y-m-d h:i:s"); // error => it is time now
+// $now= date("Y-m-d h:i:s"); // error => it is time now
 
-$couponname= filterRequest("couponname");
+// $couponname= filterRequest("couponname");
 
 
 
- getData("coupon","coupon_name = '$couponname' AND coupon_expiredate > '$now' AND coupon_count > 0");
+//  getData("coupon","coupon_name = '$couponname' AND coupon_expiredate > '$now' AND coupon_count > 0");
+//=========================test=====================
 
 //  getData("coupon","coupon_name = '$couponname' "); // for testing 
 
@@ -16,3 +17,17 @@ $couponname= filterRequest("couponname");
 
 // $couponcount= filterRequest("couponcount");
 // getData("coupon","coupon_name = '$couponname' AND coupon_expiredate > '$now' AND coupon_count = '$couponcount'");
+
+//======================= now version ==
+$now= date("Y-m-d h:i:s"); // error => it is time now
+
+$couponname= filterRequest("couponname");
+
+$usersid= filterRequest("usersid");
+
+$checkcouponrecorder = getData("couponrecorder","couponrecorder_couponname = '$couponname' AND 	couponrecorder_usersid ='$usersid'" ,null ,false);
+if($checkcouponrecorder>0) {
+    echo json_encode(array("status" => "failure"));
+}else{
+    getData("coupon","coupon_name = '$couponname' AND coupon_expiredate > '$now' AND coupon_count > 0 ");
+}
